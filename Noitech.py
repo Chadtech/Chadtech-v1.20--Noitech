a3911208fc56aa188a401a3651d6ced69e145621
ChadTech <chadstearns1@gmail.com>
2014-06-14 15:58:04 -0700

    Found some bugs in the addTo function. Mysterious bugs relating to the fade out function were discovered when random integers between 200 and 500 were arguments. It must be that some numbers do not work, but why, remains unknown to me

--- Noitech.py
+++ Noitech.py
@@ -57,11 +57,11 @@ def makeToneMitSamps(tone,dur): #Returns an array of a sine wave with frequency 
 def makeSaw(tone,dur,harmNum): #Make a saw tooth wave, at frequency TONE, for duration DUR. A saw tooth is a construction of harmonics, which approaches infinitely the saw tooth wave form. harmNum is how many of those harmonics to generate
 	outRay = []
 	inTone = float(tone)/sampleRate
-	inDur = int(float(dur)*(noteDur/oneSec)*(sampleRate))
-	for yit in range(inDur):
+	#inDur = int(float(dur)*(noteDur/oneSec)*(sampleRate))
+	for yit in range(dur):
 		outRay.append(0.)
 	for harmonic in range(1,harmNum):
-		for vapp in range(len(values)):
+		for vapp in range(len(outRay)):
 			outRay[vapp]+= amp*((-1)**(harmonic))*(math.sin(vapp*2*math.pi*inTone*harmonic)/harmonic)
 	return outRay
 
@@ -189,10 +189,15 @@ def makeEmptyArray(dur): # Makes an empty array with the length given (dur) in n
 def givDur(barNum,dur): #Returns the duration in samples, given the number of bars, number of notes per bar, and time duration of each note
 	return (noteDur/oneSec*sampleRate)*dur
 
-def addTo(whereAt,durRay,canvasRay,level=1000.): #whereAt is (WhichBar, which of noteDiv*barNum in whichbar), function adds input array to song array starting at whereAt. 
-	whereAtIn = whereAt*(noteDur/oneSec)*sampleRate
-	for vapp in range(len(durRay)):
-		canvasRay[vapp+int(whereAtIn)] += durRay[vapp] *(level/1000.)
+def addTo(durRay,canvasRay,whereAt=0,level=1000.): #whereAt is (WhichBar, which of noteDiv*barNum in whichbar), function adds input array to song array starting at whereAt. 
+	outRay=[]
+	for moment in [0]*len(canvasRay):
+		outRay.append(0.)
+	for moment in range(len(canvasRay)):
+		outRay[moment]=canvasRay[moment]
+	for moment in range(len(durRay)):
+		outRay[moment+int(whereAt)]+=int((durRay[moment]*(level/1000.)))
+	return outRay
 
 def removeFrom(whereAt,durRay,canvasRay,level=1000.): #whereAt is (WhichBar, which of noteDiv*barNum in whichbar), function adds input array to song array starting at whereAt. 
 	whereAtIn = whereAt*(noteDur/oneSec)*sampleRate
@@ -459,9 +464,6 @@ def volDrop(durRay,volPert): #Change the volume for an array as a whole values l
 	percent = 0
 	for vapp in range(len(inRay)):
 		outRay.append(inRay[vapp]*volPert)
-		if vapp%((int(len(inRay)))/100)==0:
-			percent += 1
-			print percent, '%', 'volDrop'
 	return outRay
 
 def fadeOut(durRay,beginning=0,ending=''):
@@ -470,7 +472,7 @@ def fadeOut(durRay,beginning=0,ending=''):
 	outRay=[]
 	for time in range(len(durRay)):
 		outRay.append(durRay[time])
-	for time in range(ending-beginning):
+	for time in range((ending-beginning)):
 		outRay[time+beginning]*=(((ending-beginning)-time)/float((ending-beginning)))
 	return outRay
 
@@ -631,35 +633,38 @@ def shiftSamples(durRay,shiftMag): #shiftMag must be between -1 and 1
 	return outRay
 
 def grainSynth(durRay,freqInc,grainLength,fade=True):
-	grainLength=int(grainLength)
-	inputLength=len(durRay)
-	freqAdjGrainLength=freqInc*grainLength
-	grains=[]
-	for section in range(len(durRay)/grainLength):
+	try:
+		grainLength=int(grainLength)
+		inputLength=len(durRay)
+		freqAdjGrainLength=freqInc*grainLength
+		grains=[]
+		for section in range(len(durRay)/grainLength):
+			grain=[]
+			for moment in range(int(freqAdjGrainLength)):
+				if len(durRay)>((section*grainLength)+moment):
+					grain.append(durRay[(section*grainLength)+moment])
+			grains.append(grain)
 		grain=[]
-		for moment in range(int(freqAdjGrainLength)):
-			if len(durRay)>((section*grainLength)+moment):
-				grain.append(durRay[(section*grainLength)+moment])
+		for moment in range(len(durRay)-((len(durRay)/grainLength)*grainLength)):
+			grain.append(durRay[moment+((len(durRay)/grainLength)*grainLength)])
 		grains.append(grain)
-	grain=[]
-	for moment in range(len(durRay)-((len(durRay)/grainLength)*grainLength)):
-		grain.append(durRay[moment+((len(durRay)/grainLength)*grainLength)])
-	grains.append(grain)
-	if fade:
-		for grain in range(len(grains)):
-			if len(grains[grain]):
-				grains[grain]=changeSpeed(grains[grain],freqInc)
-				grains[grain]=fadeOut(grains[grain],beginning=(len(grains[grain])-30))
-				grains[grain]=fadeIn(grains[grain],ending=30)
-	else:
-		for grain in range(len(grains)):
-			if len(grains[grain]):
-				grains[grain]=changeSpeed(grains[grain],freqInc)
-	outRay=[]
-	for grain in grains:
-		for moment in grain:
-			outRay.append(moment)
-	return outRay
+		if fade:
+			for grain in range(len(grains)):
+				if len(grains[grain]):
+					grains[grain]=changeSpeed(grains[grain],freqInc)
+					grains[grain]=fadeOut(grains[grain],beginning=(len(grains[grain])-30))
+					grains[grain]=fadeIn(grains[grain],ending=30)
+		else:
+			for grain in range(len(grains)):
+				if len(grains[grain]):
+					grains[grain]=changeSpeed(grains[grain],freqInc)
+		outRay=[]
+		for grain in grains:
+			for moment in grain:
+				outRay.append(moment)
+		return outRay
+	except:
+		print 'IT BROKE!!!!!', grainLength
 
 def grabSample(durRay,sampleLength):
 	sampleLength=int(sampleLength)
